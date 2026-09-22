3560916d7cf51c65a1529ec143f8c2f5174f2603
rifa-brasil <binancebrasil2026@gmail.com>
2026-09-23 00:26:19 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,15 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = os.getenv("ADMIN_ID")
+
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN no está configurado.")
+
+if not ADMIN_ID:
+    raise ValueError("ADMIN_ID no está configurado.")
+
+ADMIN_ID = int(ADMIN_ID)
